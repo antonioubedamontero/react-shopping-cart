a4f73ed88d1f8ef074b0ffd8ea326296c3c691e3
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-10-02 09:34:35 +0200

    totals is ready

--- src/shopping-cart/shared/total-price.tsx
+++ src/shopping-cart/shared/total-price.tsx
@@ -1,3 +1,12 @@
+import { useShoppingCartContext } from "../contexts/shopping-cart.context";
+
 export const TotalPrice = () => {
-  return <div>Total price</div>;
+  const { shoppingCartState } = useShoppingCartContext();
+
+  return (
+    <section className="bg-gray-300 px-4 py-2 rounded text-xl flex gap-2 justify-end">
+      <strong className="font-medium">Total price: </strong>
+      <span>{shoppingCartState.total}</span>
+    </section>
+  );
 };
